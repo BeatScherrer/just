abbda6b52389ffcd694bd25bb9abe6852379a1e9
Beat Scherrer <b.scherrer@mt-robot.com>
2022-08-13 15:25:06 +0200

    fixup! add error context to compile error

--- tests/error_messages.rs
+++ tests/error_messages.rs
@@ -1,37 +1,64 @@
 use super::*;
 
-test! {
-  name: expected_keyword,
-  justfile: "foo := if '' == '' { '' } arlo { '' }",
-  stderr: "
+#[test]
+fn expected_keyword() {
+  let test = Test::new();
+  let path = test.justfile_path();
+
+  test
+    .justfile("foo := if '' == '' { '' } arlo { '' }")
+    .stderr(format!(
+      "
     error: Expected keyword `else` but found identifier `arlo`
       |
-    1 | foo := if '' == '' { '' } arlo { '' }
+    1 | foo := if '' == '' {{ '' }} arlo {{ '' }}
       |                           ^^^^
+      --> {}:1:26
   ",
-  status: EXIT_FAILURE,
+      path.to_str().unwrap()
+    ))
+    .status(EXIT_FAILURE)
+    .run();
 }
 
-test! {
-  name: unexpected_character,
-  justfile: "!~",
-  stderr: "
+#[test]
+fn unexpected_character() {
+  let test = Test::new();
+  let path = test.justfile_path();
+
+  test
+    .justfile("!~")
+    .stderr(format!(
+      "
     error: Expected character `=`
       |
     1 | !~
       |  ^
+      --> {}:1:2
   ",
-  status: EXIT_FAILURE,
+      path.to_str().unwrap(),
+    ))
+    .status(EXIT_FAILURE)
+    .run();
 }
 
-test! {
-    name: argument_count_mismatch,
-    justfile: "foo a b:",
-    args: ("foo"),
-    stderr: "
+#[test]
+fn argument_count_mismatch() {
+  let test = Test::new();
+  let path = test.justfile_path();
+
+  // TODO how should this be displayed?
+  test
+    .justfile("foo a b:")
+    .stderr(format!(
+      "
       error: Recipe `foo` got 0 arguments but takes 2
       usage:
-          just foo a b
+          just foo a bench
+    {}
     ",
-    status: EXIT_FAILURE,
+      path.to_str().unwrap()
+    ))
+    .status(EXIT_FAILURE)
+    .run();
 }
